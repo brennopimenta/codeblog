b5faa4dd28402b1d6b8e74167a05638f5f7201ec
development <brennopimenta@gmail.com>
2020-12-03 12:37:35 -0200

    First commit

---
+++ src/main/java/com/spring/codeblog/model/Post.java
@@ -0,0 +1,38 @@
+package com.spring.codeblog.model;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import java.time.LocalDate;
+
+@Entity
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "tb_post")
+public class Post {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @NotBlank
+    private String titulo;
+
+    @NotBlank
+    private String autor;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING,pattern = "dd-MM-yyyy")
+    private LocalDate data;
+
+    @NotBlank
+    @Lob
+    private String texto;
+
+}
---
+++ src/main/java/com/spring/codeblog/utils/DummyData.java
@@ -0,0 +1,42 @@
+package com.spring.codeblog.utils;
+
+import com.spring.codeblog.model.Post;
+import com.spring.codeblog.repository.CodeblogRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class DummyData {
+
+    @Autowired
+    CodeblogRepository codeblogRepository;
+
+//    @PostConstruct
+    public void savePosts() {
+        List<Post> postList = new ArrayList<>();
+        Post post1 = new Post();
+        post1.setAutor("Brenno Pimenta");
+        post1.setData(LocalDate.now());
+        post1.setTitulo("Kubernets");
+        post1.setTexto("Texto de teste do post 3 jasdfalsdfahsdjkf");
+
+        Post post2 = new Post();
+        post2.setAutor("Brenno Pimenta");
+        post2.setData(LocalDate.now());
+        post2.setTitulo("JAVA");
+        post2.setTexto("Texto de teste do post 4 jasdfalsdfahsdjkf");
+
+        postList.add(post1);
+        postList.add(post2);
+
+        for (Post post: postList) {
+            Post postSaved = codeblogRepository.save(post);
+            System.out.println(postSaved.getId());
+        }
+    }
+}
